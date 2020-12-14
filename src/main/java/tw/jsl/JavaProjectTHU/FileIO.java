fb0a70475f2e1cedbdd6a88d8a7890458f596560
s08351050 <s08351050@VM-ST02-P04.thu.edu.tw>
2020-12-14 16:10:56 +0800

    update

--- src/main/java/tw/jsl/JavaProjectTHU/FileIO.java
+++ src/main/java/tw/jsl/JavaProjectTHU/FileIO.java
@@ -17,10 +17,39 @@ class FileIO {
 	}
 
 	// public methods
-
+	public void newFile () {
+		emptyWorkbook ();
+		excelFile = null;
+	}
+	public void openFile (String fileName) {
+		if (fileName == null)
+			openFile_UI ();
+		else
+			pathToFile (fileName);
+		extractExcelFromFile ();	
+	}
+	public void writeFile (String fileName) {
+		if (fileName == null)
+			saveFile_UI ();
+		else
+			pathToFile (fileName);
+		writeExcelToFile ();
+	}
 
 	// private methods
-	private void chooseFile_UI () {
+	private void saveFile_UI () {
+		if (Chooser == null) {
+			Chooser = new JFileChooser ();
+		}
+		FileNameExtensionFilter filter = new FileNameExtensionFilter ("xlsx");
+		Chooser.setFileFilter (filter);
+		int returnVal = Chooser.showSaveDialog(null);
+		if(returnVal == JFileChooser.APPROVE_OPTION) {
+			excelFile = Chooser.getSelectedFile();
+		}
+
+	}
+	private void openFile_UI () {
 		// Select file and return File object from window
 		Chooser = new JFileChooser (); 
 		FileNameExtensionFilter filter = new FileNameExtensionFilter("xlsx");
@@ -31,6 +60,11 @@ class FileIO {
 		}
 	}
 
+	private void pathToFile (String filePath) {
+		excelFile = new File (filePath);
+	}
+
+
 	private void extractExcelFromFile () {
 		try {
 			// Select file by manually selecting path
@@ -40,11 +74,26 @@ class FileIO {
 					FileInputStream infile = new FileInputStream (excelFile);
 					Book = new XSSFWorkbook (infile);
 				}
+				else {
+					excelFile = null;
+					Book = new XSSFWorkbook ();
+				}
 			}
 			if (Book == null) {
 				Book = new XSSFWorkbook ();
+				excelFile = null;
 			}
 		
+		} catch (IOException e) {
+			e.printStackTrace ();
+		}
+	}
+	private void writeExcelToFile () {
+		try {
+			FileOutputStream outfile = new FileOutputStream (excelFile);
+			Book.write (outfile);
+			outfile.close ();
+
 		} catch (IOException e) {
 			e.printStackTrace ();
 		}
--- src/test/java/tw/jsl/JavaProjectTHU/FileIOTest.java
+++ src/test/java/tw/jsl/JavaProjectTHU/FileIOTest.java
@@ -6,7 +6,7 @@ import junit.framework.TestSuite;
 
 // class test dependency
 import org.apache.poi.xssf.usermodel.*;
-//import java.io.*;
+import java.io.*;
 
 /**
  * Unit test for simple App.
@@ -33,7 +33,7 @@ public class FileIOTest
 	/**
 	 * Rigourous Test :-)
 	 */
-	public void testFileIO_newFile()
+	public void testFileIO_plainOpen()
 	{
 		FileIO excel = new FileIO ();
 		Class cls = XSSFWorkbook.class;
@@ -41,7 +41,53 @@ public class FileIOTest
 		assertTrue( excel.Book instanceof XSSFWorkbook );
 		assertTrue( cls.isInstance (excel.Book) );
 	}
+	public void testFileIO_newFile()
+	{
+		FileIO excel = new FileIO ();
+		excel.newFile ();
+		Class cls = XSSFWorkbook.class;
+		assertTrue( excel.excelFile == null );
+		assertTrue( excel.Book instanceof XSSFWorkbook );
+		assertTrue( cls.isInstance (excel.Book) );
+	}
+	public void testFileIO_openFile_str_isDirectory()
+	{
+		String filePath = "src/test/resources/dirExcel.xlsx";
+		FileIO excel = new FileIO ();
+		excel.openFile (filePath);
+		Class cls = XSSFWorkbook.class;
+		assertTrue( excel.excelFile == null );
+		assertTrue( excel.Book instanceof XSSFWorkbook );
+		assertTrue( cls.isInstance (excel.Book) );
+	}
+	public void testFileIO_openFile_str_isReal ()
+	{
+		String filePath = "src/test/resources/realExcel.xlsx";
+		FileIO excel = new FileIO ();
+		excel.openFile (filePath);
+		Class cls = XSSFWorkbook.class;
+		assertTrue( excel.excelFile.exists () );
+		assertTrue( excel.Book instanceof XSSFWorkbook );
+		assertTrue( cls.isInstance (excel.Book) );
+	}
+	public void testFileIO_writeFile_str_target ()
+	{
+		String filePath = "src/test/resources/realExcel.xlsx";
+		FileIO excel = new FileIO ();
+		excel.writeFile ("src/test/resources/targetExcel.xlsx");
+		assertTrue ( excel.excelFile.exists () );
+	}
 	/*
+	public void testFileIO_writeFile_str_nullFile()
+	{
+		String filePath = "src/test/resources/dirExcel.xlsx";
+		FileIO excel = new FileIO ();
+		excel.writeFile (filePath);
+		Class cls = XSSFWorkbook.class;
+		assertTrue( excel.Path == null );
+		assertTrue( excel.Book instanceof XSSFWorkbook );
+		assertTrue( cls.isInstance (excel.Book) );
+	}
 	public void testFileIO_nullPath()
 	{
 		FileIO excel = new FileIO (null);
@@ -57,15 +103,6 @@ public class FileIOTest
 		FileIO excel = new FileIO (filePath);
 		Class cls = XSSFWorkbook.class;
 		assertTrue( excel.Path == null );
-		assertTrue( excel.Book instanceof XSSFWorkbook );
-		assertTrue( cls.isInstance (excel.Book) );
-	}
-	public void testFileIO_isReal ()
-	{
-		String filePath = "src/test/resources/realExcel.xlsx";
-		FileIO excel = new FileIO (filePath);
-		Class cls = XSSFWorkbook.class;
-		assertTrue( excel.Path.equals (filePath) );
 		assertTrue( excel.Book instanceof XSSFWorkbook );
 		assertTrue( cls.isInstance (excel.Book) );
 	}
